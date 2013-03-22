bed83a98a66e9171c848dc1084caa2568beb13df
Andrey.Sorokin <Andrey.Sorokin@R-Style.com>
2013-03-22 14:09:16 +0400

    initial version with support publish subscribe model

--- gateaway/queue2api/scripts/msdos/run.bat
+++ gateaway/queue2api/scripts/msdos/run.bat
@@ -6,4 +6,4 @@
  )
  set CLASSPATH=!CLASSPATH!"
 rem  echo !CLASSPATH!
-java -Xms512m -Xmx1024m -classpath !CLASSPATH! ru.rstyle.si.MyServiceDemo 
+java -Xms512m -Xmx1024m -classpath !CLASSPATH! ru.rstyle.si.main.Receiver 
