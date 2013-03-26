bbec9ace4e485685a67e571a870e550196ec1d02
Andrey.Sorokin <Andrey.Sorokin@R-Style.com>
2013-03-26 16:18:54 +0400

    minor corrections

--- gateaway/api2topic/src/main/java/ru/rstyle/si/service/ParcelProcessor.java
+++ gateaway/api2topic/src/main/java/ru/rstyle/si/service/ParcelProcessor.java
@@ -2,9 +2,9 @@ package ru.rstyle.si.service;
 
 import ru.rstyle.si.domain.Parcel;
 
-public class TradeProcessor {
+public class ParcelProcessor {
 
-	public Parcel receiveTrade(Parcel t) {
+	public Parcel receiveParcel(Parcel t) {
 		System.out.println("Received the Trade using Gateway:"+t.getName());
 	
 		return t;
