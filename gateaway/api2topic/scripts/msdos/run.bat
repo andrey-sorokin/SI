fc870f012c7cd46fb5538f006f45b4a8334e6fb7
Andrey.Sorokin <Andrey.Sorokin@R-Style.com>
2013-03-26 15:38:33 +0400

    folder rename, minor bug fix

--- gateaway/api2topic/scripts/msdos/run.bat
+++ gateaway/api2topic/scripts/msdos/run.bat
@@ -6,4 +6,4 @@
  )
  set CLASSPATH=!CLASSPATH!"
 rem  echo !CLASSPATH!
-java -Dfile.encoding=UTF-8 -Duser.language=en -Duser.region=us -server -Xms512m -Xmx1024m -classpath !CLASSPATH! ru.rstyle.si.main.GatewayEndpointTest
+java -Dfile.encoding=UTF-8 -Duser.language=en -Duser.region=us -server -Xms512m -Xmx1024m -classpath !CLASSPATH! ru.rstyle.si.main.Sender
